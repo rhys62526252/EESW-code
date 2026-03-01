582927dbd792bfc722895b4ba08f365ae0b453e8
Rhys Williams <rhyswilliams@Rhyss-MBP.druidstone>
2026-03-01 10:16:27 +0000

    mk2 made with a more specialised class structure

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sqlite3
-
+from datetime import datetime
 def login(username, password):
     try:
         conn = sqlite3.connect("hospital.db")
         cursor = conn.cursor()
         cursor.execute(f"SELECT PasswordHash FROM Staff WHERE UserName = ?", (username,))
         results = cursor.fetchone()
-
+        password = hash(password)
         if results[0] == password:
             cursor.execute(f"SELECT Name FROM Staff WHERE UserName = ?", (username,))
             results = cursor.fetchone()
@@ -26,6 +26,7 @@ class accounter():
         cursor.execute(f"SELECT * FROM Staff WHERE UserName = ?", (username,))
         results = cursor.fetchone()
         print(results)
+        self._StaffID = results[0]
         self._speciality = results[2]
         self._AccessRights = results[3]
         self._RFIDTagID = results[4]
@@ -38,22 +39,47 @@ class accounter():
         return self._RFIDTagID
 
     def searchPatients(self, patientName):
-        if int(self.returnAS()) > 1:#
+
+        if int(self.returnAS()) >= 2:
             conn = sqlite3.connect("hospital.db")
             cursor = conn.cursor()
-        
-            cursor.execute(f"SELECT * FROM Patients WHERE LastName = ?", (patientName,))
-            results = cursor.fetchone()
-            try:
-                print(results[1], results[2], 'has a diagnosis of', results[6], 'at stage', results[7])
-            except:
-                print('patient records cannot be accessed')
-            
 
-username = input('username: ')
-password = input('password: ')
-accountName = login(username, password)
-if accountName != False:
-    account = accounter(accountName)
-    patientName = input('enter patient last name: ')
-    account.searchPatients(patientName)
+            cursor.execute("""
+                SELECT FirstName, LastName, Diagnosis, Stage
+                FROM Patients
+                WHERE LastName = ?
+            """, (patientName,))
+            
+            results = cursor.fetchone()
+
+            if results is None:
+                print("Patient not found.")
+                conn.close()
+                return
+
+            first, last, diagnosis, stage = results
+
+            print(first, last, "has a diagnosis of", diagnosis, "at cancer stage", stage)
+
+            action_text = f"Searched patient record: {first} {last}"
+            timestamp = datetime.now().isoformat()
+
+            cursor.execute("""
+                INSERT INTO AccessLogs (StaffID, Action, Timestamp)
+                VALUES (?, ?, ?)
+            """, (self._StaffID, action_text, timestamp))
+
+            conn.commit()
+            conn.close()
+ #           except:
+  #              print('patient records cannot be accessed')
+        else: print('Cannot access due to lack of access rights')
+            
+while True:
+    username = input('username: ')
+    password = input('password: ')
+    accountName = login(username, password)
+    if accountName != False:
+        account = accounter(accountName)
+        patientName = input('enter patient last name: ')
+        account.searchPatients(patientName)
---
+++ mk2/main.py
@@ -0,0 +1,181 @@
+import sqlite3
+from datetime import datetime
+
+class User:
+    def __init__(self, staffID, username):
+        self._staffID = staffID
+        self._username = username
+
+        conn = sqlite3.connect("hospital.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT Name, Speciality, AccessRights, RFIDTagID
+            FROM Staff
+            WHERE StaffID = ?
+        """, (self._staffID,))
+
+        result = cursor.fetchone()
+        conn.close()
+
+        if result is None:
+            raise Exception("Staff member not found")
+
+        self._name = result[0]
+        self._speciality = result[1]
+        self._accessRights = result[2]
+        self._rfidTagID = result[3]
+
+    def get_name(self):
+        return self._name
+
+    def get_access_level(self):
+        return self._accessRights
+
+
+
+
+class doctor(User):
+    def __init__(self, staffID, username):
+        super().__init__(staffID, username)
+
+    
+    def log_action(self, action_text):
+        conn = sqlite3.connect("hospital.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO AccessLogs (StaffID, Action, Timestamp)
+            VALUES (?, ?, ?)
+        """, (self._staffID, action_text, datetime.now().isoformat()))
+
+        conn.commit()
+        conn.close()
+
+    def search_patient(self, last_name):
+        conn = sqlite3.connect("hospital.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT FirstName, LastName, Diagnosis, Stage
+            FROM Patients
+            WHERE LastName = ?
+        """, (last_name,))
+
+        result = cursor.fetchone()
+        conn.close()
+
+        if result is None:
+            print("Patient not found.")
+            return None
+
+        first, last, diagnosis, stage = result
+
+        self.log_action(f"Searched patient record: {first} {last}")
+
+        print(first, last, "has a diagnosis of", diagnosis, "at stage", stage)
+        return result
+    
+    def add_patient(self):
+        conn = sqlite3.connect("hospital.db")
+        cursor = conn.cursor()
+        firstName = input('first name: ')
+        lastName = input('last name: ')
+        DateOfBirth = input('date of birth: ')
+        NHSNumber = input('NHS number ')
+        RFIDTag = input('RFID tag ID: ')
+        diagnosis = input('diganoiss: ')
+        stage = input('stage: ')
+
+        cursor.execute("""
+            INSERT INTO Patients (FirstName, LastName, DateOfBirth, NHSNumber, RFIDTagID, Diagnosis, Stage)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        """, (firstName, lastName, DateOfBirth, NHSNumber, RFIDTag, diagnosis, stage))
+        
+
+        conn.commit()
+        conn.close()
+        self.log_action(f"added patient record: {firstName} {lastName}")
+
+class Nurse(User):
+    def __init__(self, staffID, username):
+        super().__init__(staffID, username)
+    
+    def log_action(self, action_text):
+        conn = sqlite3.connect("hospital.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO AccessLogs (StaffID, Action, Timestamp)
+            VALUES (?, ?, ?)
+        """, (self._staffID, action_text, datetime.now().isoformat()))
+
+        conn.commit()
+        conn.close()
+
+    def search_patient(self, last_name):
+        conn = sqlite3.connect("hospital.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT FirstName, LastName, Diagnosis, Stage
+            FROM Patients
+            WHERE LastName = ?
+        """, (last_name,))
+
+        result = cursor.fetchone()
+        conn.close()
+
+        if result is None:
+            print("Patient not found.")
+            return None
+
+        first, last, diagnosis, stage = result
+
+        self.log_action(f"Searched patient record: {first} {last}")
+
+        print(first, last, "has a diagnosis of", diagnosis, "at stage", stage)
+        return result
+
+class Admin(User):
+    def __init__(self, staffID, username):
+        super().__init__(staffID, username)
+
+    def log_action(self, action_text):
+        conn = sqlite3.connect("hospital.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO AccessLogs (StaffID, Action, Timestamp)
+            VALUES (?, ?, ?)
+        """, (self._staffID, action_text, datetime.now().isoformat()))
+
+        conn.commit()
+        conn.close()
+
+    def search_patient(self, last_name):
+        conn = sqlite3.connect("hospital.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT FirstName, LastName, Diagnosis, Stage
+            FROM Patients
+            WHERE LastName = ?
+        """, (last_name,))
+
+        result = cursor.fetchone()
+        conn.close()
+
+        if result is None:
+            print("Patient not found.")
+            return None
+
+        first, last, diagnosis, stage = result
+
+        self.log_action(f"Searched patient record: {first} {last}")
+
+        print(first, last, "has a diagnosis of", diagnosis, "at stage", stage)
+        return result
+    
+staff = doctor(1, 'ecarter')
+staff.add_patient()
